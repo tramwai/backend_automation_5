22c062f012e5a0f715bf8a781a8b56ec4b9c7d56
tramwai <MIHAIL.HARITON@GMAIL.COM>
2023-03-12 15:24:42 -0500

    added EcelUtil Class

---
+++ src/test/java/apache_poi_excel/TestingExcelUtil.java
@@ -0,0 +1,22 @@
+package apache_poi_excel;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import utils.ExcelUtil;
+
+public class TestingExcelUtil {
+
+    private static Logger logger = LogManager.getLogger(TestingExcelUtil.class);
+    public static void main(String[] args) {
+
+        logger.info("Opening Excel file");
+        ExcelUtil.openExcelFile("ReadData", "Sheet1");
+
+        String cellValue = ExcelUtil.getValue(2, 0);
+        logger.info("Cell value is " + cellValue);
+
+        System.out.println(ExcelUtil.getValues());
+
+
+    }
+}
---
+++ src/test/java/utils/ExcelUtil.java
@@ -0,0 +1,69 @@
+package utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcelUtil {
+
+    static Logger logger = LogManager.getLogger(ExcelUtil.class);
+    private static XSSFWorkbook workbook;
+    private static XSSFSheet sheet;
+    private static XSSFRow row;
+    private static XSSFCell cell;
+    private static String filePath;
+
+    public static void openExcelFile(String fileName, String sheetName){
+        filePath = "test_data/" + fileName + ".xlsx";
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            logger.info("File " + fileName + " exists");
+            workbook = new XSSFWorkbook(fileInputStream);
+            sheet = workbook.getSheet(sheetName);
+            logger.info("Sheet " + sheetName + " exists");
+        }catch (Exception e){
+            logger.debug(fileName + " and " + sheetName + " cannot be found");
+        }
+    }
+
+
+    // getting the single value from the Excel file
+    public static String getValue(int rowNumber, int cellNUmber){
+        row = sheet.getRow(rowNumber);
+        cell = row.getCell(cellNUmber);
+        return cell.toString();
+    }
+
+    // getting al the values
+    public static List<List<String>> getValues(){
+        // creating List of List to store all the values from the excel file
+        List<List<String>> allValues = new ArrayList<>();
+
+        // creating loops for getting the rows
+        for (int r = sheet.getFirstRowNum() + 1; r <= sheet.getLastRowNum(); r++) {
+            // creating a row in the sheet
+            row = sheet.getRow(r);
+            // creating a list for storing row data
+            List<String> eachRow = new ArrayList<>();
+            // looping each cell in the corresponding row
+            for (int c = row.getFirstCellNum(); c < row.getLastCellNum(); c++) {
+                /// get each cell value and add them into eachRow list
+                eachRow.add(getValue(r,c));
+            }
+            // adding each list of row to the list of list
+            allValues.add(eachRow);
+        }
+        // returning the list of list
+        return allValues;
+    }
+
+}
